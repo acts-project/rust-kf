b88db348ed7e4794c28a460c458157206273b0e8
VanillaBrooks <brookskarlik@gmail.com>
2019-03-10 23:41:15 -0700

    added structs for sensor types

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,47 @@
 #[allow(dead_code)]
 
 pub enum SensorType{
-    Rectanlge,
+    Rectangle,
     Trapezoid,
 }
 
+pub struct Point{
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+pub struct GenericSensor{
+    p1: f32,
+    p2: f32,
+    p3: f32,
+    p4: f32,
+    sensor_type: SensorType,
+}
 pub struct Rectangle {
-    length_x: u32,
-    length_y: u32,
+    length_x: f32,
+    length_y: f32,
+}
+
+impl Rectangle {
+    pub fn new(length_x:f32, length_y:f32) -> Rectangle{
+        return Rectangle{
+            length_x: length_x,
+            length_y: length_y,
+        }
+    }
 }
 
 pub struct Trapezoid{
-    length_base_1: u32,
-    length_base_2: u32,
-    length_height: u32,
-    alpha_angle: u32,
-    beta_angle: u32,
+    length_base_1: f32,
+    length_base_2: f32,
+    length_height: f32,
+    alpha_angle: f32,
+    beta_angle: f32,
 }
 
 impl Trapezoid {
-    pub fn new(base_1: u32, base_2:u32, height:u32, alpha:u32, beta:u32) -> Trapezoid {
+    pub fn new(base_1: f32, base_2:f32, height:f32, alpha:f32, beta:f32) -> Trapezoid {
         return Trapezoid{length_base_1: base_1,
                         length_base_2: base_2,
                         length_height: height,
@@ -28,11 +50,6 @@ impl Trapezoid {
     }
 }
 
-impl Rectangle {
-    pub fn new(length_x:u32, length_y:u32) -> Rectangle{
-        return Rectangle{
-            length_x: length_x,
-            length_y: length_y,
-        }
-    }
+trait fetch_data {
+    fn data <T> (&self) -> T;
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod lib;
 #[allow(dead_code)]
 
 // takes in a global location and outputs the position relative to local scope
-fn transform_to_local() -> () {
-
+fn transform_to_local(location: lib::Point) -> () {
+    
 
 }
 
@@ -17,12 +17,20 @@ fn transform_to_global() -> () {
 }
 
 // checks if point is within sensor bounds
-fn check_if_on_sensor <T>(sensor_struct: T, sensot_type: lib::SensorType) -> () {
+fn check_if_on_sensor <T>(sensor_struct: T, sensor_type: lib::SensorType) -> () {
 
+    match sensor_type {
+        lib::SensorType::Rectangle => {
+            
+        },// rectangle
+        lib::SensorType::Trapezoid => {
 
+        }// trapezoid
+    }
 }
-
 
 fn main() {
     println!("Hello, world!");
 }
+
+
