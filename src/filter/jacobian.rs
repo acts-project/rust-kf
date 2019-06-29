cdd90d70eff4203a215358a7fc76854b16142960
VanillaBrooks <brookskarlik@gmail.com>
2019-06-28 22:47:02 -0700

    second method for linear jacobian calculation

--- src/filter/jacobian.rs
+++ src/filter/jacobian.rs
@@ -68,3 +68,83 @@ pub fn linear(
 
     return global_to_local_jacobian * transport_jac * local_to_global_jacobian
 }
+
+/// Mirrors https://gitlab.cern.ch/acts/acts-core/blob/master/Core/include/Acts/Propagator/StraightLineStepper.hpp#L307
+/// Does not include shift from state derivative calculated https://gitlab.cern.ch/acts/acts-core/blob/master/Core/include/Acts/Propagator/StraightLineStepper.hpp#L353
+/// Transport is handled outside of function.
+pub fn linear_state_derivative(
+    prev_state_vec: &Vec5
+    ) -> Mat5 {
+    
+    get_unchecked!{
+        prev_state_vec[ePHI] => phi,
+        prev_state_vec[eTHETA] => theta
+    }
+
+    let sin_phi = phi.sin();
+    let cos_phi = phi.cos();
+
+    let sin_theta = theta.sin();
+    let cos_theta = theta.cos();
+    let inv_sin_theta = theta.asin();
+
+    /*
+
+        global => local
+        parallels https://gitlab.cern.ch/acts/acts-core/blob/master/Core/include/Acts/Propagator/StraightLineStepper.hpp#L322-344
+
+    */
+    let mut jac_to_curv = Mat5x8::zeros();
+
+    change_mat_val!{
+        jac_to_curv: 8;
+        [0,0] => -sin_phi,
+        [0,1] => cos_phi,
+        [1,0] => cos_phi * cos_theta,
+        [1,1] => sin_phi * cos_theta,
+        [1,2] => sin_theta,
+        // ^^^^^^^ does not account for numerically unstable coordinate system
+        // time parameter
+        [5,3] => 1.,
+        // direction  / momentum parameters for curvilinear
+        [2,4] => sin_phi * inv_sin_theta,
+        [2,5] => cos_phi * inv_sin_theta,
+        [3,6] => -inv_sin_theta,
+        [4,7] => 1.
+    }
+
+    /*
+
+        local => global
+        parallels https://gitlab.cern.ch/acts/acts-core/blob/master/Core/include/Acts/Propagator/StraightLineStepper.hpp#L346-378
+        
+    */
+
+    let mut jac_to_global = Mat8x5::zeros();
+
+    change_mat_val!{
+        jac_to_global: 5;
+
+        [0, eLOC_0] => -sin_phi,
+        [0, eLOC_1] => -cos_phi * cos_theta,
+
+        [1, eLOC_0] => cos_phi,
+        [1, eLOC_1] => -sin_phi * cos_theta,
+
+        [2, eLOC_1] => sin_theta,
+        [3, eT] => 1.,
+
+        [4, ePHI] => -sin_theta * sin_phi,
+        [4, eTHETA] => cos_theta * cos_phi,
+
+        [5, ePHI] => sin_theta *  cos_phi,
+        [5, eTHETA] => cos_theta * sin_phi,
+
+        [6, eTHETA] => -sin_theta,
+        [7, eQOP] => 1.
+    }
+
+    let full_jacobian = jac_to_curv * jac_to_global;
+
+    return full_jacobian
+}
