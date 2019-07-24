5d184c8efdf0b8ee87c5e019c9ad2fd9566672ac
VanillaBrooks <brookskarlik@gmail.com>
2019-07-24 11:21:46 -0700

    fix smoothing data not being reversed

--- src/generate_data/structs.rs
+++ src/generate_data/structs.rs
@@ -107,7 +107,7 @@ impl Uncertainty {
         Self{
             point_std: 0.1,
             diag_std: 1.,
-            corner_std: 1.,
+            corner_std: 0.0001,
             diag_mean: 4.,
             corner_mean: 0.
         }
